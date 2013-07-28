8e5d713940984495e6dbbf0dc5a6d2ecb1efe41c
Ubuntu <ubuntu@ip-172-31-30-29.us-west-2.compute.internal>
2013-07-28 15:35:40 +0000

    Dev setup script

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash 
+
+# Install GIT 
+sudo apt-get install -y git
+sudo apt-get install -y curl
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.12
+nvm use v0.10.12
+
+# Install nvm: node-version manager
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+
+# Install jshint to allow checking of JS code within emacs
+npm install -g jshint
+
+# Install rlwrap to provide libreadline features with node
+sudo apt-get install -y rlwrap
+
+# Install emacs24
+sudo apt-add-repository -y ppa:cassou/emacs
+sudo apt-get -qq update
+sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
+# Install Heroku toolbelt
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+
+git clone https://github.com/startup-class/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
+
